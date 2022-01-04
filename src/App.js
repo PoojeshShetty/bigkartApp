09998fcf4c15bba49726e032fd13f898f615bf4e
PoojeshShetty <shettypoojesh@live.com>
2022-01-04 10:24:28 +0530

    add: routes to login , signup, home , products

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
 import Navbar from './component/navbar/Navbar';
+import { Switch, Route} from 'react-router-dom'
+import Login from './page/login/Login'
+import Signup from './page/signup/Signup'
+import Products from './page/products/Products'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <div className="App__container">
+      <Switch>
+        <Route path="/login" exact>
+          <Login />
+        </Route>
+        <Route path="/signup" exact>
+          <Signup />
+        </Route>
+        <Route path="/products" >
+          <Products />
+        </Route>
+        <Route path="/" >
+          Home
+        </Route>
+        
+      </Switch>
+      </div>
     </div>
   );
 }
---
+++ src/page/products/Products.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Products.css'
+
+function Products() {
+    return (
+        <div>
+            <div className="products__container">
+                Producst page
+            </div>
+        </div>
+    )
+}
+
+export default Products
